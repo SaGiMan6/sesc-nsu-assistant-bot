ba14d4eaa975209943e32b019868aa0d3f0a05dc
Sagiman <sasha@sagiman.ru>
2023-10-17 23:08:19 +0700

    router

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+from handlers import bot_Added_Kicked
+
 import configparser
 
 private_config = configparser.ConfigParser()
@@ -16,6 +18,8 @@ logging.basicConfig(level=logging.INFO)
 async def main():
     bot = Bot(token=private_config["token"]["BOT_TOKEN"])
     dp = Dispatcher()
+
+    dp.include_routers(bot_Added_Kicked.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
