0ecb4ac11eacceff2aaf09d6852fdd095da19d06
Sagiman <sasha@sagiman.ru>
2023-10-19 22:38:17 +0700

    new_config

--- bot.py
+++ bot.py
@@ -4,22 +4,22 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
-from handlers import bot_Added_Kicked
+# from handlers import bot_status
 
-import configparser
+import json
 
-private_config = configparser.ConfigParser()
-private_config.read("private_Config.ini")
+with open('private_config.json', 'r') as file:
+    private_config = json.load(file)
 
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def main():
-    bot = Bot(token=private_config["token"]["BOT_TOKEN"])
+    bot = Bot(token=private_config["BOT_TOKEN"])
     dp = Dispatcher()
 
-    dp.include_routers(bot_Added_Kicked.router)
+    # dp.include_routers(bot_status.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
