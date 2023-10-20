23ce1f5cdb6bc559e940b071c42c57efbfacf112
Sagiman <sasha@sagiman.ru>
2023-10-20 12:23:47 +0700

    Update bot_menu.py

--- handlers/bot_menu.py
+++ handlers/bot_menu.py
@@ -1,7 +1,8 @@
 from aiogram import Router, F
-from aiogram.enums import InputMediaType
 from aiogram.filters import Command
 from aiogram.types import Message, InputMediaPhoto, FSInputFile
+
+import datetime
 
 from menu.get_menu import get_menu
 
@@ -11,11 +12,14 @@ router = Router()
 
 @router.message(Command("menu"))
 async def cmd_menu(message: Message):
-    menu_data = get_menu()
-    data_to_upload = []
+    menu_lists = get_menu()
+    menu_to_upload = []
 
-    for i in range(len(menu_data)):
-        file = FSInputFile(menu_data[i])
-        data_to_upload.append(InputMediaPhoto(media=file))
+    for menu_list in menu_lists:
+        file = FSInputFile(menu_list)
+        if menu_list != menu_lists[-1]:
+            menu_to_upload.append(InputMediaPhoto(media=file))
+        else:
+            menu_to_upload.append(InputMediaPhoto(media=file, caption=f"Меню на {str(datetime.date.today())}"))
 
-    await message.answer_media_group(data_to_upload)
+    await message.answer_media_group(menu_to_upload)
