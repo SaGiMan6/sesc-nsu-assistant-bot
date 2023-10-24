4896b0976a3c7315a216fcfa0bf258c2e6b2e69a
Sagiman <97221557+SaGiMan6@users.noreply.github.com>
2023-10-24 17:23:12 +0700

    data_type_update

--- handlers/bot_menu.py
+++ handlers/bot_menu.py
@@ -17,7 +17,7 @@ async def preparing_menu(date):
 
     await db.execute("CREATE TABLE IF NOT EXISTS Menus (date TEXT PRIMARY KEY, id TEXT)")
 
-    cursor = await db.execute(f"SELECT date, id FROM Menus WHERE date = ?", (date,))
+    cursor = await db.execute(f"SELECT date, id FROM Menus WHERE date = ?", (str(date),))
     result = await cursor.fetchone()
 
     await db.close()
@@ -40,7 +40,7 @@ async def preparing_menu(date):
             if control_page != menu_pages[-1]:
                 media_group.append(InputMediaPhoto(media=menu_page))
             else:
-                media_group.append(InputMediaPhoto(media=menu_page, caption=f"Меню на {date}"))
+                media_group.append(InputMediaPhoto(media=menu_page, caption=f"Меню на {date.strftime(r'%d.%m.%Y')}"))
 
         if result is None:
             return media_group, True, True
@@ -61,7 +61,7 @@ async def download_cleaning(date, result, num):
 
     db = await aiosqlite.connect("data_base/menu_data_base.db")
 
-    await db.execute("INSERT INTO Menus (date, id) VALUES (?, ?)", (date, id_string))
+    await db.execute("INSERT INTO Menus (date, id) VALUES (?, ?)", (str(date), id_string))
 
     await db.commit()
     await db.close()
@@ -69,7 +69,8 @@ async def download_cleaning(date, result, num):
 
 @router.message(Command("menu"))
 async def cmd_menu(message: Message):
-    date = str(datetime.date.today())
+    date = datetime.date.today()
+    # date = datetime.date(2023, 10, 30)
     result = ""
 
     menu_to_upload, download_used, not_empty = await preparing_menu(date)
@@ -77,7 +78,7 @@ async def cmd_menu(message: Message):
     if not_empty:
         result = await message.answer_media_group(menu_to_upload)
     else:
-        await message.answer(f"Возникла ошибка при получении меню на {date}")
+        await message.answer(f"Возникла ошибка при получении меню на {date.strftime(r'%d.%m.%Y')}")
 
     if download_used:
         await download_cleaning(date, result, len(menu_to_upload))
