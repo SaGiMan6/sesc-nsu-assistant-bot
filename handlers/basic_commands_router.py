9bee6f7e9e10a4cf2407f8d444188eecf9d4723a
Sagiman <sasha@sagiman.ru>
2023-10-28 22:52:56 +0700

    logging update

--- handlers/basic_commands_router.py
+++ handlers/basic_commands_router.py
@@ -8,6 +8,10 @@ router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
+    print(f"User id: {message.from_user.id}, Username: {message.from_user.username}")
+    print(f"First name: {message.from_user.first_name}, Last name: {message.from_user.last_name}")
+    print("/start")
+
     await message.answer("""
 <b>СУНЦ НГУ Помощник</b>
 
@@ -15,17 +19,27 @@ async def cmd_start(message: Message):
 
 Вы можете использовать <i>/help</i>, чтобы получить информацию о функциях бота
 
-Наш бот для обратной связи – <i>@sesc_nsu_support_bot</i>
-Напишите ему пожалуйста, если столкнулись с проблемами в работе бота
-Также вы можете написать ему свои вопросы, предложения
+<b>(Бот на данный момент находится в разработке, функционал будет увеличиваться):</b>
     """)
 
 
 @router.message(Command("help"))
 async def cmd_start(message: Message):
+    print(f"User id: {message.from_user.id}, Username: {message.from_user.username}")
+    print(f"First name: {message.from_user.first_name}, Last name: {message.from_user.last_name}")
+    print("/help")
+    
     await message.answer("""
 <b>Команды, которые на данный момент доступны:</b>
 
 <i>/menu – Команда позволяющая узнать меню на любую дату</i>
 (Нет возможности показать меню, если на эту дату оно не было опубликовано)
+
+<b>Расширение функционала уже в разработке</b>
+Планируемый дополнительный функционал: 
+1) Просмотр расписания
+2) Рассылка с меню и расписанием
+3) Оповещение о зарядке
+
+В скором будет добавлена возможность отправлять свои вопросы, предложения и жалобы на работу бота
     """)
--- handlers/menu_router.py
+++ handlers/menu_router.py
@@ -85,6 +85,10 @@ async def send_calendar_fab(message: Message, date: datetime.date, edit: bool):
 
 @router.message(Command("menu_group"))
 async def cmd_menu_group(message: Message):
+    print(f"User id: {message.from_user.id}, Username: {message.from_user.username}")
+    print(f"First name: {message.from_user.first_name}, Last name: {message.from_user.last_name}")
+    print("/menu_group")
+
     config = {"media_group": True,
               "new_message": True}
 
@@ -93,6 +97,10 @@ async def cmd_menu_group(message: Message):
 
 @router.message(Command("menu"))
 async def cmd_menu(message: Message):
+    print(f"User id: {message.from_user.id}, Username: {message.from_user.username}")
+    print(f"First name: {message.from_user.first_name}, Last name: {message.from_user.last_name}")
+    print("/menu")
+
     config = {"media_group": False,
               "new_message": True}
 
@@ -102,6 +110,10 @@ async def cmd_menu(message: Message):
 @router.callback_query(MenuSimpleCallbackFactory.filter())
 async def callbacks_menu_page_fab(callback: CallbackQuery,
                                   callback_data: MenuSimpleCallbackFactory):
+    print(f"User id: {callback.from_user.id}, Username: {callback.from_user.username}")
+    print(f"First name: {callback.from_user.first_name}, Last name: {callback.from_user.last_name}")
+    print("Menu_callback")
+
     if callback_data.action == "change":
         config = {"media_group": False,
                   "new_message": False}
@@ -117,6 +129,10 @@ async def callbacks_menu_page_fab(callback: CallbackQuery,
 @router.callback_query(MenuCalendarCallbackFactory.filter())
 async def callbacks_menu_calendar_fab(callback: CallbackQuery,
                                       callback_data: MenuCalendarCallbackFactory):
+    print(f"User id: {callback.from_user.id}, Username: {callback.from_user.username}")
+    print(f"First name: {callback.from_user.first_name}, Last name: {callback.from_user.last_name}")
+    print("Calendar_callback")
+
     if callback_data.action == "date":
         config = {"media_group": False,
                   "new_message": True}
