3941784cd11d718bc307b6fc489e27689b8e8523
gmskdbcj <gmskdbcj@gmail.com>
2023-11-11 18:38:56 +0700

    Update morning_exercise_operations.py

--- scripts/morning_exercise_operations.py
+++ scripts/morning_exercise_operations.py
@@ -2,23 +2,24 @@ import asyncio
 
 import aiohttp
 
-from bs4 import BeautifulSoup
+from warnings import warn
 
 
-url = "https://vk.com/wall-160464793?offset=0&own=1"
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
+with open("vk_access_token.txt", mode="r") as file:
+    vk_access_token = file.read()
+vk_api_version = "5.154"
+url = f"https://api.vk.ru/method/wall.get?v={vk_api_version}&owner_id=-160464793&count=1&access_token={vk_access_token}"
 
 
 async def get_last_text_message():
-    async with aiohttp.ClientSession(headers=headers) as session:
+    async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            html = await response.text()
-            soup = BeautifulSoup(html, features="lxml")
-            soup = soup.select_one(".poster__text,.wall_post_text")
-            if soup == None:
-                return
-            else:
-                return soup.text
+            a = await response.json()
+            try:
+                a = a["response"]["items"][0]["text"]
+            except:
+                a = None
+            return a
 
 
 async def ticker(delay):
@@ -27,7 +28,9 @@ async def ticker(delay):
         await asyncio.sleep(delay)
 
 
-async def set_morning_trigger(trigger_function, delay=10):
+async def set_morning_trigger(trigger_function, delay=30):
+    if delay < 28:
+        warn("The delay is too small. Limit of wall.get usage per day may be exceeded.")
     generator = ticker(delay)
     async def get_last_text():
         return await generator.asend(None)
@@ -39,7 +42,7 @@ async def set_morning_trigger(trigger_function, delay=10):
             asyncio.gather(trigger_function(text))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     async def function(string):
         print(string)
     asyncio.run(set_morning_trigger(function))
